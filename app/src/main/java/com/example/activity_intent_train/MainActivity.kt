3b8c295cec809d4293dcfa3cfcc92ff645dd44bc
Greg <lupasco.grigori@gmail.com>
2022-07-24 23:26:21 +0300

    setting up Activity

--- app/src/main/java/com/example/activity_intent_train/MainActivity.kt
+++ app/src/main/java/com/example/activity_intent_train/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.activity_intent_train
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onClickSendMsg(view: View) {
+        val intent = Intent(this, ReceiveMessageActivity::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/example/activity_intent_train/ReceiveMessageActivity.kt
@@ -0,0 +1,11 @@
+package com.example.activity_intent_train
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReceiveMessageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recive_message)
+    }
+}
