82a1011be6540ef77c463b2edd0ec785709d79d6
Greg <lupasco.grigori@gmail.com>
2022-07-25 07:23:50 +0300

    setting up intent

--- app/src/main/java/com/example/activity_intent_train/MainActivity.kt
+++ app/src/main/java/com/example/activity_intent_train/MainActivity.kt
@@ -4,15 +4,25 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var etEnterMessage: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        etEnterMessage = findViewById(R.id.etEnterMessage)
+
     }
 
     fun onClickSendMsg(view: View) {
+        val message = etEnterMessage.text.toString()
         val intent = Intent(this, ReceiveMessageActivity::class.java)
+        intent.putExtra("msg", message)
         startActivity(intent)
     }
 }
--- app/src/main/java/com/example/activity_intent_train/ReceiveMessageActivity.kt
+++ app/src/main/java/com/example/activity_intent_train/ReceiveMessageActivity.kt
@@ -1,11 +1,21 @@
 package com.example.activity_intent_train
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class ReceiveMessageActivity : AppCompatActivity() {
+
+    private lateinit var tvMessageView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recive_message)
+
+        tvMessageView = findViewById(R.id.tvMessageView)
+
+        val msg = intent.getStringExtra("msg")
+        tvMessageView.text = msg
     }
 }
